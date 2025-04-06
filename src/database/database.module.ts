d25a83f31cd82cef2351e7102a574a54b36f67e5
Bernapai <juanemeunier@gmail.com>
2025-04-06 18:41:54 -0300

    Add Database Module

---
+++ src/database/database.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres', // Tipo de base de datos
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'Juanber123()',
+      database: 'pethub',
+      entities: [__dirname + '/../**/*.entity.ts'],
+      synchronize: true,
+    }),
+  ],
+})
+export class DatabaseModule { }

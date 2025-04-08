01f1e0c78ea01291622371ee6680d5186af93e5c
Bernapai <juanemeunier@gmail.com>
2025-04-07 21:01:20 -0300

    Add UserServices

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,40 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './users.entity';
+import { UpdateUserDto } from './dtos/updateUser.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersServices {
+
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) { }
+
+  async findAll(): Promise<User[]> {
+    return this.userRepository.find();
+  }
+
+  async findOne(id: number): Promise<User> {
+    const user = await this.userRepository.findOne({ where: { id } });
+    if (!user) {
+      throw new Error(`User with id ${id} not found`);
+    }
+    return user;
+  }
+
+  async create(user: User): Promise<User> {
+    return this.userRepository.save(user);
+  }
+
+  async update(id: number, user: UpdateUserDto): Promise<User> {
+    await this.userRepository.update(id, user);
+    return this.findOne(id);
+  }
+
+  async remove(id: number): Promise<void> {
+    await this.userRepository.delete(id);
+  }
+
+}

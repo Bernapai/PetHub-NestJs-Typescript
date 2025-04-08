d643c493b0371be2faa13b68a65ab12066014293
Bernapai <juanemeunier@gmail.com>
2025-04-08 20:54:03 -0300

    Add postControllers

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,46 @@
-import { Controller } from '@nestjs/common';
-
+import {
+    Controller,
+    Get,
+    Put,
+    Delete,
+    Param,
+    Body,
+} from '@nestjs/common';
+import { PostsService } from './posts.service';
+import { Post } from './posts.entity';
+import { UpdatePostDto } from './dtos/updatePost.dto';
+import { CreatePostDto } from './dtos/createPost.dto';
 @Controller('posts')
-export class PostsController {}
+export class PostsController {
+
+    constructor(private readonly postsService: PostsService) { }
+    @Get()
+    async findAll(): Promise<Post[]> {
+        return this.postsService.findAll();
+    }
+
+
+    @Get(':id')
+    async findOne(@Param('id') id: number): Promise<Post> {
+        return this.postsService.findOne(id);
+    }
+
+
+    @Put(':id')
+    async update(@Param('id') id: number, @Body() post: UpdatePostDto): Promise<Post> {
+        return this.postsService.update(id, post);
+    }
+
+
+    @Delete(':id')
+    async remove(@Param('id') id: number): Promise<void> {
+        return this.postsService.delete(id);
+    }
+
+
+    @Put()
+    async create(@Body() post: CreatePostDto): Promise<Post> {
+        return this.postsService.create(post);
+
+    }
+}

849ebcbc4d586966909c8a82816a58aec6e7062f
Bernapai <juanemeunier@gmail.com>
2025-04-07 21:04:19 -0300

    Add UserController

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import {
+    Controller,
+    Get,
+    Put,
+    Delete,
+    Param,
+    Body,
+} from '@nestjs/common';
+import { UsersServices } from './users.service';
+import { User } from './users.entity';
+import { UpdateUserDto } from './dtos/updateUser.dto';
+
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private readonly usersService: UsersServices) { }
+
+    @Get()
+    async findAll(): Promise<User[]> {
+        return this.usersService.findAll();
+    }
+
+    @Get(':id')
+    async findOne(@Param('id') id: number): Promise<User> {
+        return this.usersService.findOne(id);
+    }
+
+    @Put(':id')
+    async update(@Param('id') id: number, @Body() user: UpdateUserDto): Promise<User> {
+        return this.usersService.update(id, user);
+    }
+
+    @Delete(':id')
+    async remove(@Param('id') id: number): Promise<void> {
+        return this.usersService.remove(id);
+    }
+
+
+
+
+}

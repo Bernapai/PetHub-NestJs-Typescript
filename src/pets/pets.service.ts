07cdaf55c392fef58521dd39f74a77b7852feac3
Bernapai <juanemeunier@gmail.com>
2025-04-09 21:03:54 -0300

    Add petServices

--- src/pets/pets.service.ts
+++ src/pets/pets.service.ts
@@ -1,4 +1,38 @@
 import { Injectable } from '@nestjs/common';
-
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Pet } from './pets.entity';
+import { UpdatePetDto } from './dtos/updatePet.dto';
+import { CreatePetDto } from './dtos/createPet.dto';
 @Injectable()
-export class PetsService {}
+export class PetsService {
+    constructor(
+        @InjectRepository(Pet)
+        private readonly petRepository: Repository<Pet>,
+    ) { }
+
+    async findAll(): Promise<Pet[]> {
+        return this.petRepository.find();
+    }
+
+    async findOne(id: number): Promise<Pet> {
+        const pet = await this.petRepository.findOne({ where: { id } });
+        if (!pet) {
+            throw new Error(`Pet with id ${id} not found`);
+        }
+        return pet;
+    }
+
+    async create(pet: CreatePetDto): Promise<Pet> {
+        return this.petRepository.save(pet);
+    }
+
+    async update(id: number, pet: UpdatePetDto): Promise<Pet> {
+        await this.petRepository.update(id, pet);
+        return this.findOne(id);
+    }
+
+    async delete(id: number): Promise<void> {
+        await this.petRepository.delete(id);
+    }
+}

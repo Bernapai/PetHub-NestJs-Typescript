97195593448a75c0e3e0af2ad4589850ff3b5a94
Bernapai <juanemeunier@gmail.com>
2025-04-11 16:53:38 -0300

    Add eventsControllers

--- src/events/events.controller.ts
+++ src/events/events.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import {
+    Controller,
+    Get,
+    Put,
+    Delete,
+    Param,
+    Body,
+    Post
+} from '@nestjs/common';
+import { EventsService } from './events.service';
+import { Evento } from './events.entity';
+import { UpdateEventoDto } from './dtos/updateEvents.dto';
+import { CreateEventoDto } from './dtos/createEvents.dto';
 
 @Controller('events')
-export class EventsController {}
+export class EventsController {
+    constructor(private readonly eventsService: EventsService) { }
+
+    @Get()
+    async findAll(): Promise<Evento[]> {
+        return this.eventsService.findAll();
+    }
+
+    @Get(':id')
+    async findOne(@Param('id') id: number): Promise<Evento> {
+        return this.eventsService.findOne(id);
+    }
+
+    @Put(':id')
+    async update(@Param('id') id: number, @Body() evento: UpdateEventoDto): Promise<Evento> {
+        return this.eventsService.update(id, evento);
+    }
+
+    @Delete(':id')
+    async remove(@Param('id') id: number): Promise<void> {
+        return this.eventsService.delete(id);
+    }
+
+    @Post()
+    async create(@Body() evento: CreateEventoDto): Promise<Evento> {
+        return this.eventsService.create(evento);
+    }
+}

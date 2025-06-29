f0fd90a0f3971b5c18a221ec7d6282e33f0e3f77
Bernapai <juanemeunier@gmail.com>
2025-06-29 16:45:22 -0300

    Add Jwt-Auth-Guards

--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -1,6 +1,25 @@
-import { Injectable } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+import { Request } from "express";
 
 @Injectable()
-// eslint-disable-next-line @typescript-eslint/no-unsafe-call
-export class JwtAuthGuard extends AuthGuard('jwt') { }
+export class AuthGuard implements CanActivate {
+    constructor(private readonly jwtService: JwtService) { }
+
+    canActivate(context: ExecutionContext): boolean {
+        const request = context.switchToHttp().getRequest<Request>();
+        const token = request.headers.authorization?.split(" ")[1];
+
+        if (!token) {
+            return false; // No token provided
+        }
+
+        try {
+            const payload = this.jwtService.verify(token);
+            request.user = payload; // Attach user info to request
+            return true; // Token is valid
+        } catch (error) {
+            return false; // Token is invalid or expired
+        }
+    }
+}
